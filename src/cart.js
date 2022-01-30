4daa844f294133aac4c59deb012fa8f073c298e2
Toma Nicu <nikybv75fxp@yahoo.com>
2022-01-30 14:34:26 +0200

    modified id with class

--- src/cart.js
+++ src/cart.js
@@ -1,10 +1,9 @@
 window.addEventListener("load", cartItems);
 function cartItems() {
   let cart = localStorage.getItem("cart");
-  console.log(cart);
+
   let cartItems = JSON.parse(cart);
   console.log(cartItems);
-
   const productCard = cartItems
     .map(
       (product) =>
@@ -13,18 +12,19 @@ function cartItems() {
             <h5>${product.name}</h5>
             
             <div class="quantity">
-                <input type="button" value="-" id="countDown" onclick="countDown()" />
+                <input type="button" value="-" class="countDown" />
       
-                <p id="count">0</p>
+                <p class="count">1</p>
       
-                <input type="button" value="+" id="countUp" onclick="countUp()" />
+                <input type="button" value="+" class="countUp" />
             </div>
             <div class="price">
-                <span id="priceValue">€ ${product.price}</span>
+                <span class="priceValue"> ${product.price}</span>
             </div>
-            <button id="update">Update</button>
-            <button id="deleteItem"><i class="fas fa-trash-alt"></i></button>
-                    
+            <button classs="update">Update</button>
+            <button class="deleteItem"><i class="fas fa-trash-alt"></i></button>
+            <span class="itemTotalPrice">0</span>
+                  
         </div>
       
       </div>`
@@ -32,21 +32,57 @@ function cartItems() {
     .join("");
 
   document.querySelector(".cart-items").innerHTML = productCard;
+
+  //count items in cart
+  let itemNumber = cartItems.length;
+  document.querySelector(".countProducts").innerHTML = itemNumber;
+  totalProductPrice();
+
+  let plus = document.querySelector(".countUp");
+  let minus = document.querySelector(".countDown");
+  let value = Number(document.querySelector(".count").innerHTML);
+
+  // let counterParagraph = document.querySelector(".count");
+
+  plus.addEventListener("click", incrementNumber);
+  minus.addEventListener("click", decrementNumber);
+
   function incrementNumber() {
-    if (value < 10) {
+    if (value < 100) {
       value++;
     } else {
-      value = 0;
+      value = 1;
     }
-    counterParagraph.innerHTML = value;
+    document.querySelector(".count").innerHTML = value;
   }
   function decrementNumber() {
-    if (value > 0) {
+    if (value > 1) {
       value--;
     } else {
-      value = 10;
+      value = 1;
     }
 
-    counterParagraph.innerHTML = value;
+    document.querySelector(".count").innerHTML = value;
+  }
+  //count total product price
+
+  function totalProductPrice() {
+    let price = Number(document.querySelector(".priceValue").innerHTML);
+    let countItem = Number(document.querySelector(".count").innerHTML);
+    let itemPrice = Number(price) * Number(countItem);
+    document.querySelector(".itemTotalPrice").innerHTML = itemPrice;
+  }
+
+  //delete product
+  btns = document.getElementsByClassName("deleteItem");
+  for (let i = 0; i <= btns.length - 1; i++) {
+    btns[i].addEventListener("click", deleteItem);
+
+    function deleteItem(event) {
+      let deleteBtn = event.target;
+      if (deleteBtn.classList.contains("fas")) {
+        deleteBtn.parentNode.parentNode.remove();
+      }
+    }
   }
 }
