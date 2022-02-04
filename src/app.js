12e553971933a2c35d9c7023f776656d2d808511
Toma Nicu <nikybv75fxp@yahoo.com>
2022-02-04 19:39:57 +0200

    Home page complete

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ async function fetchProducts() {
     "https://61e06cc763f8fc0017618752.mockapi.io/products"
   );
   let products = await result.json();
+  console.log(products);
 
   let productCardsString = products
     .map(
@@ -13,16 +14,105 @@ async function fetchProducts() {
             <h5>${product.name}</h5>
             <img src=${product.imageURL} alt="image missing" />
             <div class="price">
-                <span id="priceOf">Price: €  </span>
-                <span id="priceValue">${product.price}</span>
+                <span class="priceOf">Price: € </span>
+                <span class="priceValue">${product.price}</span>
             </div>
-            <div class="details">
-                <a href="detail.html?id=${product.id}">Details</a>
-                <a href="#"><i class="fas fa-cart-plus"></i></a>
-            </div>
+            <a href="detail.html?id=${product.id}"><button class="details">Details</button></a>
+            
         </div>`
     )
     .join("");
 
   document.querySelector(".products-cards-grid").innerHTML = productCardsString;
+}
+
+let bikes = document.getElementById("bikes");
+
+bikes.addEventListener("click", fetchProductsBikes);
+
+async function fetchProductsBikes() {
+  let resultBikes = await fetch(
+    "https://61e06cc763f8fc0017618752.mockapi.io/bikes"
+  );
+
+  let productsBikes = await resultBikes.json();
+
+  let productCardsStringmtb = productsBikes
+    .map(
+      (product) =>
+        `<div class="product-card">
+                <h5>${product.name}</h5>
+                <img src=${product.imageURL} alt="image missing" />
+                <div class="price">
+                    <span class="priceOf">Price: € </span>
+                    <span class="priceValue">${product.price}</span>
+                </div>
+                <a href="detail.html?id=${product.id}"><button class="details">Details</button></a>
+                
+            </div>`
+    )
+    .join("");
+
+  document.querySelector(".products-cards-grid").innerHTML =
+    productCardsStringmtb;
+}
+
+let protection = document.getElementById("protection");
+protection.addEventListener("click", fetchProductsProtection);
+
+async function fetchProductsProtection() {
+  let resultProtection = await fetch(
+    "https://61e06cc763f8fc0017618752.mockapi.io/protection"
+  );
+
+  let productsProtection = await resultProtection.json();
+
+  let productCardsStringmtb = productsProtection
+    .map(
+      (product) =>
+        `<div class="product-card">
+                <h5>${product.name}</h5>
+                <img src=${product.imageURL} alt="image missing" />
+                <div class="price">
+                    <span class="priceOf">Price: € </span>
+                    <span class="priceValue">${product.price}</span>
+                </div>
+                <a href="detail.html?id=${product.id}"><button class="details">Details</button></a>
+                
+            </div>`
+    )
+    .join("");
+
+  document.querySelector(".products-cards-grid").innerHTML =
+    productCardsStringmtb;
+}
+
+let wear = document.getElementById("wear");
+wear.addEventListener("click", fetchProductsWear);
+
+async function fetchProductsWear() {
+  let resultWear = await fetch(
+    "https://61e06cc763f8fc0017618752.mockapi.io/wear"
+  );
+
+  let productsWear = await resultWear.json();
+
+  let productCardsStringmtb = productsWear
+    .map(
+      (product) =>
+        `<div class="product-card">
+                <h5>${product.name}</h5>
+                <img src=${product.imageURL} alt="image missing" />
+                <div class="price">
+                    <span class="priceOf">Price: € </span>
+                    <span class="priceValue">${product.price}</span>
+                </div>
+                <a href="detail.html?id=${product.id}"><button class="details">Details</button></a>
+                
+            </div>`
+    )
+    .join("");
+
+  document.querySelector(".products-cards-grid").innerHTML =
+    productCardsStringmtb;
 }
